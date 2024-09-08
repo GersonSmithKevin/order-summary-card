85c9b78f8fb8ea6f2699116dc1021f48dcc67cd9
GersonSmithKevin <Gersoncamacho49@gmail.com>
2024-09-08 18:40:12 -0500

    feat: create EncabezadoConSubtítulo component

---
+++ src/EncabezadoConSubtítulo.js
@@ -0,0 +1,15 @@
+import './EncabezadoConSubtítulo.css';
+//
+function EncabezadoConSubtítulo({ }) {
+  const texto = "Creation of";
+  return (
+    <div class="header-container">
+      <h1 class="header-title">Order Summary</h1>
+      <p class="header-subtitle">You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!</p>
+    </div>
+  )
+}
+
+
+
+export default EncabezadoConSubtítulo;
