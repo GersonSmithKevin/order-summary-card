5c72515f57d71890a677e0fce62a02b0c52aa04a
GersonSmithKevin <Gersoncamacho49@gmail.com>
2024-09-08 18:39:28 -0500

    feat: create ImagenCabecera component with corresponding CSS

---
+++ src/ImagenCabecera.js
@@ -0,0 +1,16 @@
+import './ImagenCabecera.css';
+//
+function ImagenCabecera() {
+  return (
+    <>
+      <div className="illustrative-header-container">
+        <img src="illustration-hero.svg" alt="Person enjoying music" className="illustrative-header-image" />
+      </div>
+    </>
+
+  )
+}
+
+
+
+export default ImagenCabecera;
